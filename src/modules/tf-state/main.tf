d32560a3e28ef5e96742f3a2d4f13284ad62b685
xi2481-santosh <santosh.nagargoje@xebia.com>
2022-12-19 18:08:54 +0530

    action code

---
+++ src/modules/tf-state/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+///varibale file
+
+
+variable "bucket_name" {
+  description = "Remote S3 Bucket Name"
+  type        = string
+  validation {
+    condition     = can(regex("^([a-z0-9]{1}[a-z0-9-]{1,61}[a-z0-9]{1})$", var.bucket_name))
+    error_message = "Bucket Name must not be empty and must follow S3 naming rules."
+  }
+}
